bd6f6bed57a2406786528bbee285dc5490b5ffce
Sebastian Roberts <tiansebas09@gmail.com>
2023-04-29 17:38:23 -0600

    Added the Help command

--- Main.py
+++ Main.py
@@ -14,6 +14,7 @@ Discord_Token = os.getenv("Discord_Token")
 api_key = os.getenv("Riot_Key")
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+bot.remove_command('help')
 
 @bot.event
 async def on_ready():
@@ -33,12 +34,12 @@ async def on_member_remove(member):
 async def ping(ctx):
     await ctx.reply(f"Pong! {round(bot.latency * 1000)}ms")
 
-@bot.command(aliases=['8ball', 'test'])
+@bot.command(aliases=['8ball'])
 async def eightball(ctx, *, question = None):
     if question == None:
         await ctx.send("Please put in a question.")
         return
-    
+
     responses = ["As I see it, yes.", "Ask again later.", "Better not tell you now.", "Can't predict now.", "Concentrate and ask again.",
                  "Don't count on it.", "It is certain.", "It is decidedly so.", "Most likely.", "My reply is no.", "My sources say no.",
                  "Outlook not so good.", "Outlook good.", "Reply hazy, try again.", "Signs point to yes.", "Very doubtful.", "Without a doubt.",
@@ -46,22 +47,8 @@ async def eightball(ctx, *, question = None):
     await ctx.send(f"**Question: **{question}\n**Answer:** {random.choice(responses)}")
 
 @bot.command()
-async def embed(ctx, member:discord.Member = None):
-    if member == None:
-        member = ctx.author
-
-    name = member.display_name
-    pfp = member.display_avatar
-
-    embed = discord.Embed(title="This is my Embed", description="This is a test embed, I'm gonna change the information later", color=discord.Color.random())
-    embed.set_author(name=f"{name}")
-    embed.set_thumbnail(url=f"{pfp}")
-    embed.add_field(name="This is 1 field", value="This field is just a value")
-    embed.add_field(name="This is 2 field", value="This field is inline True", inline=True)
-    embed.add_field(name="This is 3 field", value="This field is inline False", inline=False)
-    embed.set_footer(text=f"{name} made this Embed")
-
-    await ctx.send(embed=embed)
+async def help(ctx):
+    await ctx.send("**!help** - Brings up the help menu full of all the commands\n\n**!eightball** - A simple eightball command that gives back several different answer\n`Alias: 8ball`\n\n**!lolProfile <Summoner Name>** - Uses the Riot API to get the level and rank\n`Alias: lol`\n\nMore commands might be added soon. If you have any ideas, please suggest them to the bot creator.")
 
 @bot.command(aliases=['lol'])
 async def lolProfile(ctx, summonerName = None):
