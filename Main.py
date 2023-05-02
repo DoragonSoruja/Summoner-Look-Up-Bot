34b643cfbc011e9093b9fd6a54fd8cfb8eaa236c
Sebastian Roberts <tiansebas09@gmail.com>
2023-05-02 14:17:14 -0600

    Added command that gives the stats from the last match played

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
 import random
+import math
+from datetime import datetime
 import discord
 from version import GetLatestVersion
 from discord.ext import commands
@@ -48,12 +50,13 @@ async def eightball(ctx, *, question = None):
 
 @bot.command()
 async def help(ctx):
-    await ctx.send("**!help** - Brings up the help menu full of all the commands\n\n**!eightball** - A simple eightball command that gives back several different answer\n`Alias: 8ball`\n\n**!lolProfile <Summoner Name>** - Uses the Riot API to get the level and rank\n`Alias: lol`\n\nMore commands might be added soon. If you have any ideas, please suggest them to the bot creator.")
+    await ctx.send("**!help** - Brings up the help menu full of all the commands\n\n**!eightball** - A simple eightball command that gives back several different answer\n`Alias: 8ball`\n\n**!lolProfile <Summoner Name>** - Uses the Riot API to get the level and rank\n`Alias: lol`\n\n**!lastMatch <Summoner Name>** - Gives you stats on the last match played.\n`Alias: last`\n\nMore commands might be added soon. If you have any ideas, please suggest them to the bot creator.")
 
 @bot.command(aliases=['lol'])
 async def lolProfile(ctx, summonerName = None):
     if summonerName == None:
         await ctx.send("Please enter a Summoner Name.")
+        return
 
     api = requests.get(f'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={api_key}')
     info = api.text
@@ -64,7 +67,8 @@ async def lolProfile(ctx, summonerName = None):
         second_info = second_api.text
         summonerInfoByID = json.loads(second_info)
     except:
-        await ctx.send("Please enter in a valid summoner name.")
+        await ctx.send('Please enter in a valid summoner name.\n(If you summoner name contains a space, put it in double quotation marks `"Summoner Name"`)')
+        return
 
     embed = discord.Embed(title=summonerInfo['name'], description=f"The League profile of {summonerInfo['name']}", color=discord.Color.random())
     embed.set_thumbnail(url=f"https://ddragon.leagueoflegends.com/cdn/{GetLatestVersion()}/img/profileicon/{summonerInfo['profileIconId']}.png")
@@ -84,18 +88,23 @@ async def lolProfile(ctx, summonerName = None):
 
     await ctx.send(embed=embed)
 
-@bot.command()
+@bot.command(aliases=['last', 'lastlol'])
 async def lastMatch(ctx, summonerName = None):
     if summonerName == None:
         await ctx.send("Please enter a Summoner Name.")
+        return
 
     api = requests.get(f'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={api_key}')
     info = api.text
     summonerInfo = json.loads(info)
 
-    second_api = requests.get(f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{summonerInfo['puuid']}/ids?start=0&count=20&api_key={api_key}")
-    second_info = second_api.text
-    matchRecords = json.loads(second_info)
+    try:
+        second_api = requests.get(f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{summonerInfo['puuid']}/ids?start=0&count=20&api_key={api_key}")
+        second_info = second_api.text
+        matchRecords = json.loads(second_info)
+    except:
+        await ctx.send('Please enter in a valid summoner name.\n(If you summoner name contains a space, put it in double quotation marks `"Summoner Name"`)')
+        return
 
     third_api = requests.get(f'https://americas.api.riotgames.com/lol/match/v5/matches/{matchRecords[0]}?api_key={api_key}')
     third_info = third_api.text
@@ -109,8 +118,30 @@ async def lastMatch(ctx, summonerName = None):
         if playerInfo['summonerName'].lower() == summonerName.lower():
             targetedPlayer = playerInfo
 
-    # Implement the rest of the command
+    datetime_Start = datetime.fromtimestamp(latestMatch['info']['gameCreation'] / 1000)
+    date = datetime_Start.strftime("%m/%d")
+    timeStart = datetime_Start.strftime("%H:%M")
 
-    await ctx.send(targetedPlayer['championName'])
+    datetime_End = datetime.fromtimestamp(latestMatch['info']['gameEndTimestamp'] / 1000)
+    timeEnd = datetime_End.strftime("%H:%M")
+
+    match_duration = targetedPlayer['challenges']['gameLength'] / 60
+
+    embedColor = discord.Color.green() if targetedPlayer['win'] else discord.Color.red()
+    matchStatus = "Victory" if targetedPlayer['win'] else "Defeat"
+
+    KDA = f"{targetedPlayer['kills']}/{targetedPlayer['deaths']}/{targetedPlayer['assists']}"
+
+    embed = discord.Embed(title="Last Match", description=f"This match happened at {date}", color=embedColor)
+    embed.set_thumbnail(url=f"https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/{targetedPlayer['championId']}.png")
+    embed.add_field(name="__Champion__", value=f"{targetedPlayer['championName']}", inline=False)
+    embed.add_field(name="__Match Duration__", value=f"{round(match_duration)} minutes", inline=True)
+    embed.add_field(name="__Time Started__", value=f"{timeStart}", inline=True)
+    embed.add_field(name="__Time Ended__", value=f"{timeEnd}", inline=True)
+    embed.add_field(name="__KDA__", value=f"{KDA}", inline=True)
+    embed.add_field(name="__Damage Dealt__", value=f"{targetedPlayer['totalDamageDealtToChampions']}", inline=True)
+    embed.add_field(name="__Match Status__", value=f"{matchStatus}", inline=False)
+
+    await ctx.send(embed=embed)
 
 bot.run(Discord_Token)
